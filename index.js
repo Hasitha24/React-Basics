84a34afdfd1f42ca1dc4716ee66726e6733c90e6
Hasitha24 <hasithak002@gmail.com>
2023-08-13 21:51:22 +0530

    Newly created React program

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 import React from "react"
 import ReactDOM  from "react-dom"
-// import Header from "./Header"
-import MainContent from "./MainContent"
 import Footer from "./Footer"
 
 function Page(){
 return(
     <div>
-        <MainContent />
+        
         <Footer />
 <h1>Hasitha</h1>
     </div>
